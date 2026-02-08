868a5969e1d4bd36ef1bb78e5ef1dcb08c18259f
Ricky5163 <liedsondias1957@gmail.com>
2026-02-08 17:52:47 +0000

    config alterado

--- js/config.js
+++ js/config.js
@@ -2,5 +2,6 @@
 window.APP_CONFIG = {
   SUPABASE_URL: "https://eqklkfrxotoizpuacznc.supabase.co",
   SUPABASE_ANON_KEY: "CeyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImVxa2xrZnJ4b3RvaXpwdWFjem5jIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzAzMDAxMTAsImV4cCI6MjA4NTg3NjExMH0.Ex1LHdLN8Kfnu3ySY1JH7NUC9AM-TqXLnBiA56qE9Ow",
-  SITE_URL: "https://stalwart-squirrel-7da5f2.netlify.app"
+SITE_URL: "https://droidunlock.site"
+
 };
